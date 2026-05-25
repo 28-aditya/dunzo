a04f78e2b36986257d6fdbae1079f5b01b933640
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-05-25 20:12:17 +0530

    Version 2.0.1

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JS connected");
+
+const theme_button = document.getElementById("theme-toggle");
+theme_button.addEventListener("click", function(){
+
+    console.log("theme toggled");
+    document.body.classList.toggle("light-mode");
+
+});
+
